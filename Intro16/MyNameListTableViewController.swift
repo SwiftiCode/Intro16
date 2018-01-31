f65aff745d4299d8a3026ffa4ffc7252eb558bd2
SwiftiCoder1 <swifticoder1@renovatios-macbook-pro.local>
2018-01-31 12:53:47 +0800

    update to swift 4

--- Intro16/MyNameListTableViewController.swift
+++ Intro16/MyNameListTableViewController.swift
@@ -31,19 +31,19 @@ class MyNameListTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int {
         
         return 1
     }
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return myNameList.count
     }
 
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("myNameListCell", forIndexPath: indexPath)
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myNameListCell", for: indexPath)
 
         // Configure the cell...
         let singleName = myNameList[indexPath.row]
